d40befedd2e6d045bba3576533891dba7aa5e799
DodoTheWise <dojaphd2@gmail.com>
2024-12-26 19:48:43 +0100

    Didn't think about that... (#1552)
    * Fix `rename_to_minecraft_name` check and add check for empty keys (in social category)
    
    * Whoops, Java doesn't belong here
    
    * Fix maintenance check
    
    * Add empty key check for every key (in some way)
    
    * Fix some outputs and hopefully make action fail again when JSON is invalid
    
    * Make comment_needed also True if PR was only updated with a new commit aswell
    
    * Logic error
    
    * Enhance empty key check
    
    * Add more debugging for wildcard check
    
    * Idk why but it seems to have failed before hopefully works now tho
    
    * Idk why but it seems to have failed before hopefully works now tho
    
    * Regex error
    
    * Well, didn't think about that...
    
    ---------
    
    Co-authored-by: DoJapHD <contact@dojaphd.com>
    Co-authored-by: DoJapHD <63593457+DoJapHD@users.noreply.github.com>

--- .github/actions/validate-manifest/validate.py
+++ .github/actions/validate-manifest/validate.py
@@ -149,6 +149,11 @@ def main():
 
         if 'chat' in data and 'message_formats' in data['chat']:
             message_format = data['chat']['message_formats']
+            if isinstance(message_format, list):
+                if len(message_format) == 1:
+                    message_format = message_format[0]
+                else:
+                    error += f'**message_format** has the wrong format. Please recheck the [example manifest](https://github.com/LabyMod/server-media/blob/master/docs/Manifest.md#chat-object).'
             template_regex = r'^§[a-f0-9](?P<level>\d+)( \||§8 \|) §[a-f0-9](?P<sender>[a-zA-Z0-9_]{2,16})§r§7: §f(?P<message>.*)$'
             if re_match(template_regex, message_format):
                 comment += f'- It seems you\'re using the **template regex** for chat message! Please make sure it is the right regex for **your server**!'
