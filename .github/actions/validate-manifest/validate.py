1dd31f1e4903ce23db464a7bc8624db4d160300c
DoJapHD <contact@dojaphd.com>
2025-03-07 20:26:55 +0100

    Now also check in string not only the exact same string

--- .github/actions/validate-manifest/validate.py
+++ .github/actions/validate-manifest/validate.py
@@ -285,22 +285,24 @@ def check_server_online_state(ip: str, wildcards: list):
     wildcard_string += f'\nPlease make sure it is an [actual wildcard](https://en.wikipedia.org/wiki/Wildcard_DNS_record).\n'
     if wildcard_comment:
         post_comment(wildcard_string, 'comments')
+
     if 'motd' in response or 'version' in response:
         maintenance_tagged = False
         if 'motd' in response:
             for line in response['motd']['clean']:
                 line_content = line.lower()
-                if line_content in ['maintenance', 'wartung', 'wartungen', 'mantenimiento', 'wartungsarbeiten']:
+                if any(keyword in line_content for keyword in ['maintenance', 'wartung', 'wartungen', 'mantenimiento', 'wartungsarbeiten']):
                     maintenance_tagged = True
-                else:
-                    print(f'No maintenance found in MOTD')
+
         if 'version' in response:
             version_name = response['version'].lower()
-            if version_name in ['maintenance', 'wartung', 'wartungen', 'mantenimiento', 'wartungsarbeiten']:
+            if any(keyword in version_name for keyword in ['maintenance', 'wartung', 'wartungen', 'mantenimiento', 'wartungsarbeiten']):
                 maintenance_tagged = True
 
         if maintenance_tagged:
             post_comment(f'The server {ip} **is in maintenance**.\n {offline_text}')
+        else:
+            print(f'No maintenance found in MOTD nor version')
 
 
 def comment_needed():
